e951af72981ef86bdfb39e5a8465f776b1d88f5a
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-10 18:16:32 -0500

    nav file added

---
+++ views/partials/nav.php
@@ -0,0 +1,15 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/config.php"; ?>
+<div>
+    <!-- TODO: Add authorizations to this routes -->
+
+    <a href="/index.php">Home</a>
+    <br>
+    <a href="">Browse Items</a>
+    <br>
+    <a href="">Order Hitory</a>
+    <br>
+    <a href="">Shopping Cart</a>
+    <br>
+    <a href="">Inventory</a>
+    
+</div>
