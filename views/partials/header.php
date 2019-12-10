aa850db7715ed6c00170a22de3eabfd5661f6c9b
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-10 18:19:19 -0500

    header completed

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,3 +1,4 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/config.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php $title ?> </title>
+    <title> Online Shop | Home Page </title>
 
-
-    <style>
-        <?php require_once assets("style/style.css"); ?>
+    <!-- TODO: Fix this style sheet -->
+    <style >
+        <?php require assets("style/style.css") ?>
     </style>
 
 </head>
