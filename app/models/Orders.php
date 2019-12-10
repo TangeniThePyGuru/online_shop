265193bad59aa692e19f59df964042b727e927a9
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-10 18:16:54 -0500

    orders model added

---
+++ app/models/Orders.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+require_once ROOT_PATH .  "app/models/BaseModel.php";
+
+class Item extends BaseModel
+{
+    // TODO: Create orders table
+    protected $table = 'orders';
+
+}
