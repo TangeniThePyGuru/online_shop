fedf8bb8c73f28315e9d3773b0c33173c98b2d0e
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-11 11:25:40 -0500

    md5 hashing algorithm added

--- config/auth.php
+++ config/auth.php
@@ -1,13 +1,27 @@
 <?php
 
-/*
-    TODO: Implement login
-*/
+use App\Models\Client;
+
+$client = new Client();
+
+function register($name, $email, $password, $address){
+    global $client;
+    return $client->insert([$name, $email, hash('md5', $password), $address]);
+}
 
 // login
 function login($email, $password)
 {
-    # code...
+    global $client;
+    $user = $client->query_db("SELECT * FROM clients where  email = $email AND password = ${hash('md5',$password)}");
+
+    if($user) {
+        // TODO: start a new session
+    } else {
+        // TODO: flush message => wrong credentials
+    }
+
+    // redirect to appropriate page
 }
 
 /*
@@ -38,5 +52,14 @@ function begin_session()
 function end_session()
 {
     # code...
+}
+// TODO: is user logged_in?
+function logged_in(){
+    # code...
+}
+
+// TODO: who is the currnt logged in user's full profile
+function logged_in_user(){
+    # code....
 }
 
