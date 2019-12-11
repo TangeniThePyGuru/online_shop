c6db25df6ab156e765efdfc599cbb9eea5594569
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-11 10:55:00 -0500

    added new models to models file

--- app/models.php
+++ app/models.php
@@ -3,3 +3,5 @@
 require_once ROOT_PATH . 'app/models/Client.php';
 require_once ROOT_PATH . 'app/models/Item.php';
 require_once ROOT_PATH . 'app/models/Orders.php';
+require_once ROOT_PATH . 'app/models/Session.php';
+require_once ROOT_PATH . 'app/models/Cart.php';
