9bfeca5e21fa9649db65d33acbe886d98ecb9110
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-10 19:42:11 -0500

    route for add item to cart completed

--- app/controllers/item.controller.php
+++ app/controllers/item.controller.php
@@ -5,10 +5,19 @@ require_once app('models/Item.php');
 
 if (get_request('insert')):
     #....
+    die('lets insert an item in the inventory');
 elseif (get_request('delete')):
     #....
-else : # else update
+    die('lets update an item in the inventory');
+elseif (get_request('edit_item')): # else update
     #....
-endif
+    die('lets add an item in the inventory');
+elseif (post_request('add_to_cart')) : # else update
+    #....
+    die('lets update an item in the cart');
+else:
+    die(var_dump('else block'));
+endif;
+
 
 ?>
