e53bb5f9b29394bef83c332af6a4c3d724037b2d
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-13 14:27:20 -0500

    new methods added to cart class

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -5,19 +5,55 @@ namespace App\Models;
 
 class Cart
 {
-    // TODO: the table is the current authed user's id
     protected $table;
 
     public function __construct()
     {
-        $this->table = Session::getInstance();
+        // nitialize the table
+        $this->table = $_SESSION['cart'];
     }
     
     public function get_cart(){
-        return $this->table;
+        if (!$this->cart_is_empty()) :
+            return $this->table;
+        else :
+            return null;
+        endif;
     }
-    // TODO: To be tested
-    function add_to_cart($key, $quantity){
-        array_push($this->table, [$key => $quantity]);
+    
+
+    public function add_to_cart($key, $quantity){
+        if (isset($_SESSION['cart'][$key])) :
+           return $_SESSION['cart'][$key] += $quantity;
+        else :
+           return $_SESSION['cart'][$key] = $quantity;
+        endif;
+    }
+
+    public function items_in_cart()
+    {
+        $items = [];
+        $itm = new Item();
+        // die(var_dump($this->table));
+        foreach($this->table as $key => $item):
+            // die(var_dump($itm->get_one($key)));
+           array_push($items, $key);
+        endforeach;
+
+        return $itm->get_in_range($items);
+    }
+
+    public function quantities()
+    {
+        $q = [];
+        foreach($this->table as $key => $item):
+            array_push($q, $item);
+        endforeach;
+
+        return $q;
+    }
+
+    public function cart_is_empty(){
+        return !isset($_SESSION['cart']);
     }
 }
