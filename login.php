73a5c61d343f7ae889cbe629c69bb7ce23e1b7d2
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-13 01:03:11 -0500

    some styling updates

--- login.php
+++ login.php
@@ -5,13 +5,16 @@ require_once "config/config.php";
 ?>
 <?php extend_layout(); ?>
 
-<?php
+<div id="dash">
 
-if ($_GET['role'] == 1){
-    require_once view('auth/admin_login.php');
-} else {
-    require_once view('auth/client_login.php');
-}
-?>
+    <?php
 
+    if ($_GET['role'] == 1) {
+        require_once view('auth/admin_login.php');
+    } else {
+        require_once view('auth/client_login.php');
+    }
+    ?>
+
+</div>
 <?php close_layout(); ?>
