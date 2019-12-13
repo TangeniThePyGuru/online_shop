522f92500c33e595c432ad40a4bba40849917b21
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-12 20:40:37 -0500

    pages updated

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,15 +1,14 @@
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/config.php"; ?>
-<div>
+<div id="nav">
     <!-- TODO: Add authorizations to this routes -->
-
     <a href="/index.php">Home</a>
-    <br>
+    <!-- <br> -->
     <a href="/views/items/browse_items.php">Browse Items</a>
-    <br>
+    <!-- <br> -->
     <a href="/views/orders/make_an_order.php">Order Hitory</a>
-    <br>
+    <!-- <br> -->
     <a href="/views/items/shopping_cart.php">Shopping Cart</a>
-    <br>
+    <!-- <br> -->
     <a href="/views/inventory/index.php">Inventory</a>
 
 </div>
