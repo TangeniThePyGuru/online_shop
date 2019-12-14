27ef898f3e52ba1aa9eb558ef2464a586c9890c1
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-13 20:07:25 -0500

    auth done logout

--- config/auth.php
+++ config/auth.php
@@ -1,4 +1,5 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/'. 'config/config.php');
 
 use App\Models\Client;
 use App\Models\Session;
@@ -13,7 +14,6 @@ function register($name, $email, $password, $address){
     return $client->insert([$name, $email, hash('md5', $password), $address]);
 }
 
-// login
 function login($email, $password)
 {
     global $client;
@@ -31,40 +31,44 @@ function login($email, $password)
     } else {
         return null;
     } 
-
-    // redirect to appropriate page
-    redirect_to(view('items/browse_items.php'));
 }
 
 // logout
-// TODO: Test logout
+
 function logout()
 {
     end_session();
     redirect_to('/index.php');
 }
 
-// kill session
+
 function end_session()
 {
     global $session;
     return $session->destroy();
 }
-// is user logged_in?
 
 function is_logged_in(){
     global $session;
-    return isset($session['logged_in_user']);
+    return isset($_SESSION['logged_in_user']);
 }
 
-// who is the currnt logged in user's full profile
+function is_admin()
+{
+    if (is_logged_in() && logged_in_user()['admin']):
+        return true;
+    else:
+        return false;
+    endif;
+}
+
+// TEST: who is the currnt logged in user's full profile
 function logged_in_user(){
     global $client;
     global $session;
     if (is_logged_in()){
-        return $client->get_one($session['logged_in_user']);
+        return mysqli_fetch_assoc($client->get_one($session->__get('logged_in_user')));
     }
-    
     return null;
 }
 
