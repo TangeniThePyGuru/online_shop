7c54cc2917b2a76508b1f38b0c6a8638ae83c881
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-12-13 20:20:38 -0500

    nav item added to nav bar

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,14 +1,40 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/config.php"; ?>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/config.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . '/' . "config/auth.php";
+?>
 <div id="nav">
     <!-- TODO: Add authorizations to this routes -->
     <a href="/index.php">Home</a>
     <!-- <br> -->
     <a href="/views/items/browse_items.php">Browse Items</a>
     <!-- <br> -->
-    <a href="/views/orders/make_an_order.php">Order Hitory</a>
     <!-- <br> -->
     <a href="/views/items/shopping_cart.php">Shopping Cart</a>
     <!-- <br> -->
-    <a href="/views/inventory/index.php">Inventory</a>
+    <?php
+    if (is_admin()) :
+        ?>
+        <a href="/views/inventory/index.php">Inventory</a>
+    <?php
+    endif;
+    ?>
+
+    <?php
+    if (is_logged_in()) :
+        ?>
+        <a href="/views/orders/make_an_order.php">Order Hitory</a>
+        <a href="/app/controllers/auth.controller.php?logout=1">Logout</a>
+    <?php
+    endif;
+    ?>
+
+    <?php
+    if (!is_logged_in()) :
+    ?>
+        <a href="/views/clients/client_registration.php">Register</a>
+    <?php
+    endif;
+    ?>
+
 
 </div>
